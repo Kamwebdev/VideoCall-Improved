e801945be4a45e859c48914b518c765a0adff5c0
Kamwebdev <30120978+Kamwebdev@users.noreply.github.com>
2022-06-12 23:22:16 +0200

    Add: https support, token-based room creation

--- server.js
+++ server.js
@@ -1,31 +1,62 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+
+const fs = require('fs');
+// cat chain cert public cert > sslcert/server.key
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+
+const server = require('https').createServer(credentials, app);
 const io = require('socket.io')(server);
-const port = process.env.PORT || 4000;
-const {v4:uuidv4} = require('uuid');
-const {ExpressPeerServer} = require('peer')
-const peer = ExpressPeerServer(server , {
-  debug:true
+const port = process.env.PORT || 8443;
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer')
+const peer = ExpressPeerServer(server, {
+	debug: true
 });
+
+const secret = "<token>"
+let rooms = []
+
+//Routes
 app.use('/peerjs', peer);
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.get('/' , (req,res)=>{
-  res.send(uuidv4());
+
+app.get('/', (req, res) => {
+	res.sendStatus('403');
 });
-app.get('/:room' , (req,res)=>{
-    res.render('index' , {RoomId:req.params.room});
+app.get('/join/:room', (req, res) => {
+	if (rooms.includes(req.params.room)) {
+		res.render('index', { RoomId: req.params.room });
+	} else {
+		res.sendStatus('403');
+	};
 });
-io.on("connection" , (socket)=>{
-  socket.on('newUser' , (id , room)=>{
-    socket.join(room);
-    socket.to(room).broadcast.emit('userJoined' , id);
-    socket.on('disconnect' , ()=>{
-        socket.to(room).broadcast.emit('userDisconnect' , id);
-    })
-  })
+app.get('/create/:secret', (req, res) => {
+	if (req.params.secret == secret) {
+		uuid = uuidv4();
+		rooms.push(uuid);
+		//res.send(uuid);
+		res.redirect('/join/' + uuid);
+	} else {
+		res.sendStatus('403');
+	};
+});
+
+//Socket
+io.on("connection", (socket) => {
+	socket.on('newUser', (id, room) => {
+		socket.join(room);
+		socket.to(room).broadcast.emit('userJoined', id);
+		socket.on('disconnect', () => {
+			socket.to(room).broadcast.emit('userDisconnect', id);
+		})
+	})
 })
-server.listen(port , ()=>{
-  console.log("Server running on port : " + port);
+
+//Start server
+server.listen(port, () => {
+	console.log("Server running on port : " + port);
 })
